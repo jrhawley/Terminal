cd9523ca5ed67af747080cbdc2fed645697d65b8
James Hawley <jamesrichardhawley@gmail.com>
2019-07-08 11:46:50 -0400

    Adding Sleuth R package to RNAseq env

--- Anaconda/RNAseq.sh
+++ Anaconda/RNAseq.sh
@@ -4,4 +4,11 @@ conda activate RNAseq
 conda install snakemake-minimal
 conda install r-ggplot2
 conda install kallisto
+conda install r-sleuth
+
+conda install bioconda::rseqc
+conda install bioconda::sambamba
+conda install bioconda::multiqc
+conda install bioconda::fastqc
+conda install bioconda::trim-galore
 
